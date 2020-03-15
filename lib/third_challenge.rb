521eeb06520bfefc03517137cf096db13d32fb2a
Romena Murgor <romenamurgor@gmail.com>
2020-03-15 22:52:32 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,18 +1,4 @@
-def second_challenge
-  epic_tragedy = {		  epic_tragedy = {
-    :montague => {		    :montague => {
-      #your key/value pairs here!		      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-      }, 		      }, 
-    :capulet => {		    :capulet => {
-      #your key/value pairs here!		        :patriarch => {},
-      }}		        :matriarch => {},
-        :heroine => {},
-        :heroine_friends => []
-      }
-  }
+
 
 
 
